bcb4f1d4dc4899e48240ad5c0f4b81cd4353d3b2
poonamsaxena123 <saxena2111poonam@gmail.com>
2025-06-24 16:19:09 +0530

    add deploy code

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,12 +1,15 @@
 #!/bin/bash
 
 
-set -e
+#!/bin/bash
 
+set -e  # Exit immediately if a command exits with a non-zero status
+
+# Apply database migrations
 python manage.py makemigrations
 python manage.py migrate
 
-
+# Create superuser if it doesn't exist
 echo "
 from django.contrib.auth import get_user_model
 User = get_user_model()
@@ -27,5 +30,83 @@ if not User.objects.filter(email=email).exists():
     )
 " | python manage.py shell
 
+# Collect static files
+python manage.py collectstatic --noinput
 
-python manage.py runserver 0.0.0.0:8000
+# Start Gunicorn for production
+exec gunicorn your_project_name.wsgi:application --bind 0.0.0.0:8000
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+# set -e
+
+# python manage.py makemigrations
+# python manage.py migrate
+
+
+# echo "
+# from django.contrib.auth import get_user_model
+# User = get_user_model()
+
+# email = '$DJANGO_SUPERUSER_EMAIL'
+# username = '$DJANGO_SUPERUSER_USERNAME'
+# first_name = '$DJANGO_SUPERUSER_FIRST_NAME'
+# last_name = '$DJANGO_SUPERUSER_LAST_NAME'
+# password = '$DJANGO_SUPERUSER_PASSWORD'
+
+# if not User.objects.filter(email=email).exists():
+#     User.objects.create_superuser(
+#         email=email,
+#         username=username,
+#         first_name=first_name,
+#         last_name=last_name,
+#         password=password
+#     )
+# " | python manage.py shell
+
+
+# python manage.py runserver 0.0.0.0:8000
