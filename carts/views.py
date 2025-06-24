18f24f890be8b9e95dc9fb6cde67700865049c02
poonamsaxena123 <saxena2111poonam@gmail.com>
2025-06-24 17:43:44 +0530

    new secrate add2

--- carts/views.py
+++ carts/views.py
@@ -54,6 +54,7 @@ def remove_cart_item(request,product_id):
     product=get_object_or_404(Product,id=product_id)
     cart_item=CartItem.objects.get(product=product,cart=cart)
     cart_item.delete()
+   
     return redirect('cart')
 
 
